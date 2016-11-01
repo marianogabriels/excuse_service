0ea1be7dbe68d718b3a8b6b64fe8fbfb49479a29
Mariano Schmidt <marianogabriels@gmail.com>
2016-11-01 12:06:11 -0300

    New escusas

--- app.rb
+++ app.rb
@@ -6,7 +6,9 @@ class App < Sinatra::Base
       "Es un desarollo que lleva su tiempo, tenemos que evaluarlo con el equipo antes",
       "Estamos cerrando otros desarollos, antes lo tengo que charlar con el equipo",
       "Lo vamos a hablar con el gerente de tecnologia",
+      "Hable más fuerte que tengo una toalla",
       "Hay que esperar que se propaguen los dns",
+      "Nuestros monos no están capacitados para ese trabajo",
       "No me anda el click",
       "No estoy en la máquina"
     ]
