06fe1676d3bb19e92654d1de9bbef2f229e79417
Mariano Schmidt <marianogabriels@gmail.com>
2016-11-01 11:51:42 -0300

    Removed

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
 require 'sinatra'
 
-get '/' do
-  escusas =  [
-    "Es un desarollo que lleva su tiempo, tenemos que evaluarlo con el equipo antes",
-    "Estamos cerrando otros desarollos, antes lo tengo que charlar con el equipo",
-    "Lo vamos a hablar con el gerente de tecnologia",
-    "Manu no esta",
-    "Hay que esperar que se propaguen los dns",
-    "No me anda el click",
-    "No estoy en la máquina"
-  ]
-  escusas << "El equipo esta de vacaciones" if Time.now.month <= 2 #si es enero/febrero
+class App < Sinatra::Base
+  get '/' do
+    escusas =  [
+      "Es un desarollo que lleva su tiempo, tenemos que evaluarlo con el equipo antes",
+      "Estamos cerrando otros desarollos, antes lo tengo que charlar con el equipo",
+      "Lo vamos a hablar con el gerente de tecnologia",
+      "Hay que esperar que se propaguen los dns",
+      "No me anda el click",
+      "No estoy en la máquina"
+    ]
+    escusas << "El equipo esta de vacaciones" if Time.now.month <= 2 #si es enero/febrero
 
-  escusas.sample
+    escusas.sample
+  end
 end
