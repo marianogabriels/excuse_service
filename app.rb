42f1f426e4929ba2bb540b45b349d0d5d8f941f6
= <=>
2016-11-01 12:09:03 -0300

    Fix typo. Add excuses

--- app.rb
+++ app.rb
@@ -2,16 +2,21 @@ require 'sinatra'
 
 class App < Sinatra::Base
   get '/' do
-    escusas =  [
+    excuses =  [
       "Es un desarollo que lleva su tiempo, tenemos que evaluarlo con el equipo antes",
       "Estamos cerrando otros desarollos, antes lo tengo que charlar con el equipo",
       "Lo vamos a hablar con el gerente de tecnologia",
       "Hay que esperar que se propaguen los dns",
       "No me anda el click",
-      "No estoy en la máquina"
+      "No estoy en la máquina",
+      "Está al teléfono, me querés dejar un número de contacto?",
+      "Está en reunión",
+      "WebExperto? Desconozco",
+      "Estamos corriendo los últimos test",
+      "No sabría decirte, soy tercerizado"
     ]
-    escusas << "El equipo esta de vacaciones" if Time.now.month <= 2 #si es enero/febrero
+    excuses << "El equipo esta de vacaciones" if Time.now.month <= 2 #si es enero/febrero
 
-    escusas.sample
+    excuses.sample
   end
 end
